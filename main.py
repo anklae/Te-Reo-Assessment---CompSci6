020ef9393dad56df281677cfb39d90eae180cbdb
Noah Duffin <104875517+anklae@users.noreply.github.com>
2022-05-13 02:23:58 +0000

    code is in shambles, was working perfectly yesterday, now i have to completely revamp the question code because replit shits out if the words in the input statement is too long, i hate replit

--- main.py
+++ main.py
@@ -29,32 +29,35 @@ def respo():
 
 def question_hab():
   score = 0
-  
-  question1 = input("Question 1: What is the Te Reo word for Hello: A: Wharepaku B: Karekau C: Kia ora D: Takaro ").lower()
+  print("question 1:What is the Te Reo word for Hello: A:Wharepaku B:Karekau C:Kia ora D:Takaro: ")
+  question1 = input.lower()
   if question1 == "c" or question1 == "kia ora":
     score += 1 
     print("Correct!")
-    
+    print()
   else:
     print("Incorrect, The answer is Kia Ora ")
+    print()
   
-  question2 = input("Question 2: What is the Te Reo word for food: A: Tukituki B: Kai C: Aporo D: Hanawiti ").lower()
+  print("Question 2")
+  question2 = input("Q2 What is the Te Reo word for food: A:Tukituki B:Kai C:Aporo D:Hanawiti ").lower()
   if question2 == "b" or question2 == "kai":
     score += 1 
     print("Correct!")
   else: 
     print("Incorrect, The Answer is Kai") 
   
-  question3 = input("Question 3: What is the Te Reo word for Sea Food A:Kai Moana B: aporo C:tepu D:taraka").lower()
+  print("question 3")
+  question3 = input("Q3: What is the Te Reo word for Sea Food A:Kai Moana B: aporo C:tepu D:taraka").lower()
   if question3 == "a" or question3 == "kai moana":
     score += 1 
     print("Correct!")
   else: 
     print("Incorrect, The answer is kai moana ") 
   
-  
-  question4 = input("Question 4:What is the Te Reo word for Breakfast A:watea B:punetēpu C:taurewa D:").lower()
-  if question4 == "D" or question4 == "Breakfast":
+  print("Question 4")
+  question4 = input("What is the Te Reo word for Breakfast A:watea B:punetēpu C:aroha D:parakuihi").lower()
+  if question4 == "D" or question4 == "parakuihi":
     score += 1
     
     print("Correct!")
@@ -68,9 +71,47 @@ def question_hab():
   else: 
     print("Incorrect, The Answer is ") 
 
+  question6 = input("What is the Te Reo word for").lower()
+  if question6 == "" or question6 == "":
+    score += 1 
+    print("Correct!")
+  else: 
+    print("Incorrect, The Answer is ") 
 
+  question7 = input("What is the Te Reo word for").lower()
+  if question7 == "" or question7 == "":
+    score += 1 
+    print("Correct!")
+  else: 
+    print("Incorrect, The Answer is ") 
+  
+  question8 = input("What is the Te Reo word for").lower()
+  if question8 == "" or question8 == "":
+    score += 1 
+    print("Correct!")
+  else: 
+    print("Incorrect, The Answer is ") 
+
+  question9 = input("What is the Te Reo word for").lower()
+  if question9 == "" or question9 == "":
+    score += 1 
+    print("Correct!")
+  else: 
+    print("Incorrect, The Answer is ") 
+
+  question10 = input("What is the Te Reo word for").lower()
+  if question10 == "" or question10 == "":
+    score += 1 
+    print("Correct!")
+  else: 
+    print("Incorrect, The Answer is ") 
     
-
+  question11 = input("What is the Te Reo word for").lower()
+  if question11 == "" or question11 == "":
+    score += 1 
+    print("Correct!")
+  else: 
+    print("Incorrect, The Answer is ") 
 #Greet the user and ask them if they would like to partake in the quiz
 statement_generator("Welcome to the Te Reo Maori test!", "*")
 print()

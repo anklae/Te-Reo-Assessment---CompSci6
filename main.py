2bfa474d5a5a561d614d597572e5aa9c63da9b37
Noah Duffin <104875517+anklae@users.noreply.github.com>
2022-05-18 01:00:05 +0000

    removed the print statement in all the question_hab input variables due to replit issue, print statements are now above the input variables

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Functions go here
+
+#This function adds a border to a selected statement to give it more style and "pazaz"
 def statement_generator(statement, decoration):
 
     sides = decoration * 3
@@ -29,8 +31,8 @@ def respo():
 
 def question_hab():
   score = 0
-  print("question 1:What is the Te Reo word for Hello: A:Wharepaku B:Karekau C:Kia ora D:Takaro: ")
-  question1 = input.lower()
+  print("Q1: What is the Te Reo word for Hello?: A:wharepaku B:karekau C:kia ora D:takaro")
+  question1 = input().lower()
   if question1 == "c" or question1 == "kia ora":
     score += 1 
     print("Correct!")
@@ -38,81 +40,151 @@ def question_hab():
   else:
     print("Incorrect, The answer is Kia Ora ")
     print()
+
   
-  print("Question 2")
-  question2 = input("Q2 What is the Te Reo word for food: A:Tukituki B:Kai C:Aporo D:Hanawiti ").lower()
+  print("Q2: What is the Te Reo word for Food?: A:tukituki B:kai C:Aporo D:hanawiti ")
+  question2 = input().lower()
   if question2 == "b" or question2 == "kai":
     score += 1 
     print("Correct!")
+    print()
   else: 
     print("Incorrect, The Answer is Kai") 
+    print()
   
-  print("question 3")
-  question3 = input("Q3: What is the Te Reo word for Sea Food A:Kai Moana B: aporo C:tepu D:taraka").lower()
+  print("Q3: What is the Te Reo word for Sea Food? A:kai moana B: aporo C:tepu D:taraka")
+  question3 = input().lower( )
   if question3 == "a" or question3 == "kai moana":
     score += 1 
     print("Correct!")
+    print()
   else: 
     print("Incorrect, The answer is kai moana ") 
-  
-  print("Question 4")
-  question4 = input("What is the Te Reo word for Breakfast A:watea B:punetēpu C:aroha D:parakuihi").lower()
-  if question4 == "D" or question4 == "parakuihi":
+    print()
+ 
+  print("Q4: What is the Te Reo word for Breakfast? A:watea B:punetēpu C:aroha D:parakuihi")
+  question4 = input().lower()
+  if question4 == "d" or question4 == "parakuihi":
     score += 1
-    
     print("Correct!")
+    print()
   else: 
-    print("Incorrect, The Answer is ") 
+    print("Incorrect, The Answer is parakuihi") 
+    print()
+
+  print("Q5: What is the Te Reo word for Lunch? A:pene B:mokowhiti C:tina D:kai")
+  question5 = input().lower()
+  if question5 == "c" or question5 == "tina":
+    score += 1 
+    print("Correct!")
+    print()
   
-  question5 = input("What is the Te Reo word for").lower()
-  if question5 == "" or question5 == "":
-    score += 1 
-    print("Correct!")
   else: 
-    print("Incorrect, The Answer is ") 
+    print("Incorrect, The Answer is tina ") 
+    print()
 
-  question6 = input("What is the Te Reo word for").lower()
-  if question6 == "" or question6 == "":
+  print("Q6: What is the Te Reo word for Mom: A:mama B: karaihe C:petipeti D: hautoa ") 
+  question6 = input().lower()
+  if question6 == "a" or question6 == "mama":
     score += 1 
     print("Correct!")
+    print()
   else: 
-    print("Incorrect, The Answer is ") 
-
-  question7 = input("What is the Te Reo word for").lower()
-  if question7 == "" or question7 == "":
-    score += 1 
-    print("Correct!")
-  else: 
-    print("Incorrect, The Answer is ") 
+    print("Incorrect, The Answer is Mama") 
+    print()
   
-  question8 = input("What is the Te Reo word for").lower()
-  if question8 == "" or question8 == "":
+  print("Q7: What is the Te Reo word for Dad: A:whanau B: tepu C:aporo D:papa")
+  question7 = input().lower()
+  if question7 == "d" or question7 == "papa":
     score += 1 
     print("Correct!")
+    print()
   else: 
-    print("Incorrect, The Answer is ") 
+    print("Incorrect, The Answer is Papa") 
+    print()
+  
+  print("Q8: What is the Te Reo word for Family?: A:motokā B:whanau C: mokomoko D: KAMARAMA")
+  question8 = input().lower()
+  if question8 == "b" or question8 == "whanau":
+    score += 1 
+    print("Correct!")
+    print()
+  else: 
+    print("Incorrect, The Answer is Whanau ") 
+    print()
 
-  question9 = input("What is the Te Reo word for").lower()
-  if question9 == "" or question9 == "":
+  print("Q9: What is the Te Reo word for Cousin A:riu B: matimati C:whanaunga D:maunga ")
+  question9 = input().lower()
+  if question9 == "c" or question9 == "whanaunga":
     score += 1 
     print("Correct!")
+    print()
   else: 
-    print("Incorrect, The Answer is ") 
+    print("Incorrect, The Answer is whanaunga") 
+    print()
 
-  question10 = input("What is the Te Reo word for").lower()
-  if question10 == "" or question10 == "":
+  print("Q10: What is the Te Reo word for house?: A:kainga B:whare C:hamipere D:tiihi tiihi")
+  question10 = input().lower()
+  if question10 == "b" or question10 == "whare":
     score += 1 
     print("Correct!")
+    print()
   else: 
-    print("Incorrect, The Answer is ") 
+    print("Incorrect, The Answer is Whare ") 
+    print()
     
-  question11 = input("What is the Te Reo word for").lower()
-  if question11 == "" or question11 == "":
+  print("Q11: What is the Te Reo word for Toilet: A: oro B:karekau C:kāore he aha D:wharepaku")
+  question11 = input().lower()
+  if question11 == "D" or question11 == "wharepaku":
     score += 1 
     print("Correct!")
+    print()
+  else: 
+    print("Incorrect, The Answer is Wharepaku  ") 
+    print()
+
+    print("Q12: What is the Te Reo word for : ")
+  question12 = input().lower()
+  if question12 == "" or question12 == "":
+    score += 1 
+    print("Correct!")
+    print()
   else: 
     print("Incorrect, The Answer is ") 
-#Greet the user and ask them if they would like to partake in the quiz
+    print()
+  
+  print("Q13: What is the Te Reo word for ")
+  question13 = input().lower()
+  if question13 == "" or question13 == "":
+    score += 1 
+    print("Correct!")
+    print()
+  else: 
+    print("Incorrect, The Answer is ") 
+    print()
+
+  print("Q14: What is the Te Reo word for ")
+  question14 = input().lower()
+  if question14 == "" or question14 == "":
+    score += 1 
+    print("Correct!")
+    print()
+  else: 
+    print("Incorrect, The Answer is ") 
+    print()
+
+  print("Q15: What is the Te Reo word for ")
+  question15 = input().lower()
+  if question15 == "" or question15 == "":
+    score += 1 
+    print("Correct!")
+    print()
+  else: 
+    print("Incorrect, The Answer is ") 
+    print()
+  
+  
+    #Greet the user and ask them if they would like to partake in the quiz
 statement_generator("Welcome to the Te Reo Maori test!", "*")
 print()
 print("This test has 15 multiple choice questions")
